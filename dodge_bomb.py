1fd97dfca2b235d117213b06a931189ca9c49fb9
c0a22010 <c0a220106d@edu.teu.ac.jp>
2023-04-25 14:08:04 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,7 +8,6 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-
     bb_image = pg.Surface((20, 20))
     pg.draw.circle(bb_image, (255, 0, 0), (10, 10), 10)
     bb_image.set_colorkey((0, 0, 0))
@@ -22,6 +21,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_image, [600, 200])
 
         pg.display.update()
         clock.tick(1000)
